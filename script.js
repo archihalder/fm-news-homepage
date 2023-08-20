0f4b24e0adafe7dcd4bfd00c3f263d719da3ad01
Archi Halder <archihalder23026558@gmail.com>
2023-08-20 23:40:02 +0530

    fixed hamburger menu and added comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var isOpen = 0;
+var isOpen = 0; // check if the nav is open
 
-function changeStyle() {
+// to change the navigation style
+function changeNavStyle() {
     var a = document.querySelectorAll(".links a");
     a.forEach(i => {
         if(isOpen == 0) {
@@ -16,27 +17,40 @@ function changeStyle() {
     })
 }
 
+// to change the hamburger style
+function changeBarStyle() {
+    var b1 = document.getElementById("bar1");
+    var b2 = document.getElementById("bar2");
+    var b3 = document.getElementById("bar3");
+
+    if(isOpen == 0) {
+        b1.style.transform = "translateY(8px) rotate(45deg)";
+        b2.style.visibility = "hidden";
+        b3.style.transform = "translateY(-9px) rotate(-45deg)";
+    }
+    else {
+        b1.style.transform = "initial";
+        b2.style.visibility = "visible";
+        b3.style.transform = "initial";
+    }
+}
+
+// toggle the hamburger menu and what happens if we toggle it
 function toggleMenu() {
     const cover = document.querySelector(".cover");
     const links = document.querySelector(".links");
 
     links.classList.toggle("change");
     cover.classList.toggle("covering");
-    changeStyle();
+    changeNavStyle();
+    changeBarStyle();
     isOpen = !isOpen;
 }
 
-const screenWidth = window.innerWidth;
-const links = document.querySelectorAll(".links a");
-
-links.forEach(i => {
-    if(screenWidth < 750){
-        i.addEventListener('click', toggleMenu);
-    }
-})
-
+// if the hamburger menu is open and we click on the cover (shaded region), it must close 
 const cover = document.querySelector(".cover");
 cover.addEventListener('click', toggleMenu);
 
+// click the menu
 const menu = document.querySelector(".hamburger-menu");
 menu.addEventListener('click', toggleMenu);
